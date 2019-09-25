4f20f1e44150b4479b9d96185bff58cc49f2aee6
NobuGaga <biweixiong@yinhandeiMac.local>
2019-09-25 14:43:10 +0800

    2019-09-25_Update SkillEditorManager

--- Assets/Editor/Script/CSharp/SkillEditorManager.cs
+++ Assets/Editor/Script/CSharp/SkillEditorManager.cs
@@ -5,31 +5,42 @@ using UnityEngine;
 public static class SkillEditorManager{
 
 	private static GameObject m_model = null;
-	private static string m_modelPath = string.Empty;
+    private static SkillEditorMono m_modelScript = null;
+    private static string m_modelPath = string.Empty;
 
 	private const string ModelPrefabPath = "Assets/Editor/Asset/Prefab";
 	private const string ModelPrefabExtension = "prefab";
 	private const string EditScenePath = "Assets/Editor/Scene/EditScene.unity";
 	private const string ExitScenePath = "Assets/Editor/Scene/EditScene.unity";
 
+    private static bool isEditorMode {
+        get { return m_modelPath != string.Empty; }
+    }
+
     public static void OpenPrefab() {
 		string prefabPath = EditorUtility.OpenFilePanel("模型 Prefab 路径", ModelPrefabPath, ModelPrefabExtension);
 		if (prefabPath == null || prefabPath == string.Empty || prefabPath == m_modelPath)
 			return;
-		m_modelPath = prefabPath;
+        if (!isEditorMode)
+            EditorApplication.ExecuteMenuItem("Window/Layouts/SkillEditor");
+        EditorSceneManager.OpenScene(EditScenePath);
+        m_modelPath = prefabPath;
 		if (m_model)
 			Object.DestroyImmediate(m_model);
-		EditorSceneManager.OpenScene(EditScenePath);
 		GameObject prefab = PrefabUtility.LoadPrefabContents(m_modelPath);
 		m_model = Object.Instantiate(prefab);
-        m_model.AddComponent<SkillEditorMono>();
-        SkillEditorWindow.Open();
-        Selection.activeGameObject = m_model;
         PrefabUtility.UnloadPrefabContents(prefab);
+        m_modelScript = m_model.AddComponent<SkillEditorMono>();
+        Selection.activeGameObject = m_model;
+        SkillEditorWindow.Open();
 	}
 
     public static void RevertScene() {
+        if (!isEditorMode)
+            return;
+        m_model = null;
         m_modelPath = string.Empty;
         EditorSceneManager.OpenScene(ExitScenePath);
-	}
+        EditorApplication.ExecuteMenuItem("Window/Layouts/Default");
+    }
 }
