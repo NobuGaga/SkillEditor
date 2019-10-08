ccc8125c91b634607b9333a676dd40b5322a27ab
NobuGaga <biweixiong@yinhandeiMac.local>
2019-10-08 20:38:02 +0800

    2019-10-08_Update Code Style

--- Assets/Editor/Script/CSharp/SkillEditor/Controller/SkillEditorController.cs
+++ Assets/Editor/Script/CSharp/SkillEditor/Controller/SkillEditorController.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using System.IO;
 using System.Collections.Generic;
 
-public static class SkillEditorController
-{
+public static class SkillEditorController {
 
     private static GameObject m_model = null;
 
@@ -15,8 +14,7 @@ public static class SkillEditorController
 
     private static double m_lastTime;
 
-    public static void Start(string prefabPath)
-    {
+    public static void Start(string prefabPath) {
         Reset();
         GameObject prefab = PrefabUtility.LoadPrefabContents(prefabPath);
         m_model = Object.Instantiate(prefab);
@@ -24,20 +22,18 @@ public static class SkillEditorController
         Selection.activeGameObject = m_model;
         PrefabUtility.UnloadPrefabContents(prefab);
         SetAllAnimationClip();
-        SkillEditorAnimationConfig.AnimationClips = m_animationClips.ToArray();
-        m_isGenericClip = SkillEditorAnimationConfig.GenericState();
+        SkillEditorAnimationModel.AnimationClips = m_animationClips.ToArray();
+        m_isGenericClip = SkillEditorAnimationModel.GenericState();
         InitAnimation();
         SkillEditorScene.RegisterSceneGUI();
-        SkillEditorWindow.SetDisplayData(SkillEditorAnimationConfig.AnimationClipNames, SkillEditorAnimationConfig.AnimationClipIndexs);
+        SkillEditorWindow.SetDisplayData(SkillEditorAnimationModel.AnimationClipNames, SkillEditorAnimationModel.AnimationClipIndexs);
         SkillEditorWindow.Open();
     }
 
-    private static void SetAllAnimationClip()
-    {
+    private static void SetAllAnimationClip() {
         string[] fileNames = Directory.GetFiles(SkillEditorConfig.ClipGroupFullPath);
         m_animationClips.Clear();
-        for (int index = 0; index < fileNames.Length; index++)
-        {
+        for (int index = 0; index < fileNames.Length; index++) {
             if (fileNames[index].Contains(".meta") || !fileNames[index].Contains("@") ||
                 !(fileNames[index].Contains(".fbx") || fileNames[index].Contains(".FBX")))
                 continue;
@@ -49,8 +45,7 @@ public static class SkillEditorController
         }
     }
 
-    private static void InitAnimation()
-    {
+    private static void InitAnimation() {
         if (!m_isGenericClip)
             return;
         Animator animator = m_model.GetComponent<Animator>();
@@ -60,15 +55,13 @@ public static class SkillEditorController
         RemoveAllAnimatorTransition();
     }
 
-    private static void RemoveAllAnimatorTransition()
-    {
+    private static void RemoveAllAnimatorTransition() {
         string sourcePath = SkillEditorConfig.GetAnimatorControllerPath(m_model.name);
         string copyPath = SkillEditorConfig.GetAnimatorControllerCopyPath(m_model.name);
         AnimatorController animatorController = AssetDatabase.LoadAssetAtPath<AnimatorController>(sourcePath);
         File.Copy(SkillEditorTool.ProjectPathToFullPath(sourcePath), copyPath);
         AnimatorControllerLayer[] layers = animatorController.layers;
-        for (int layerIndex = 0; layerIndex < layers.Length; layerIndex++)
-        {
+        for (int layerIndex = 0; layerIndex < layers.Length; layerIndex++) {
             AnimatorStateMachine mainMachine = layers[layerIndex].stateMachine;
             if (mainMachine == null || mainMachine.states == null)
                 continue;
@@ -76,8 +69,7 @@ public static class SkillEditorController
             ChildAnimatorStateMachine[] subMachines = mainMachine.stateMachines;
             if (subMachines == null)
                 continue;
-            for (int machineIndex = 0; machineIndex < subMachines.Length; machineIndex++)
-            {
+            for (int machineIndex = 0; machineIndex < subMachines.Length; machineIndex++) {
                 AnimatorStateMachine subMachine = subMachines[machineIndex].stateMachine;
                 if (subMachine == null)
                     continue;
@@ -87,11 +79,9 @@ public static class SkillEditorController
         AssetDatabase.SaveAssets();
     }
 
-    private static void RemoveAnimatorMachineTransition(AnimatorStateMachine machine)
-    {
+    private static void RemoveAnimatorMachineTransition(AnimatorStateMachine machine) {
         ChildAnimatorState[] states = machine.states;
-        for (int stateIndex = 0; stateIndex < states.Length; stateIndex++)
-        {
+        for (int stateIndex = 0; stateIndex < states.Length; stateIndex++) {
             AnimatorState state = states[stateIndex].state;
             if (state == null || state.transitions == null)
                 continue;
@@ -101,23 +91,20 @@ public static class SkillEditorController
         }
     }
 
-    public static void SetAnimationClipData(int index)
-    {
-        SkillEditorAnimationConfig.SetCurrentAnimationClip(index);
+    public static void SetAnimationClipData(int index) {
+        SkillEditorAnimationModel.SetCurrentAnimationClip(index);
     }
 
-    public static void Play()
-    {
+    public static void Play() {
         m_lastTime = EditorApplication.timeSinceStartup;
         EditorApplication.update += Update;
         if (m_isGenericClip)
-            SkillEditorAnimator.Play(SkillEditorAnimationConfig.SelectAnimationClip);
+            SkillEditorAnimator.Play(SkillEditorAnimationModel.SelectAnimationClip);
         else
-            SkillEditorClip.Play(m_model, SkillEditorAnimationConfig.SelectAnimationClip);
+            SkillEditorClip.Play(m_model, SkillEditorAnimationModel.SelectAnimationClip);
     }
 
-    private static void Update()
-    {
+    private static void Update() {
         if ((m_isGenericClip && SkillEditorAnimator.IsPlayOver) || (!m_isGenericClip && SkillEditorClip.IsPlayOver))
             Stop();
         double currentTime = EditorApplication.timeSinceStartup;
@@ -129,18 +116,14 @@ public static class SkillEditorController
             SkillEditorClip.Update(deltaTime);
     }
 
-    public static void Stop()
-    {
+    public static void Stop() {
         EditorApplication.update -= Update;
     }
 
-    public static void Reset()
-    {
-        if (m_isGenericClip && m_model != null)
-        {
+    public static void Reset() {
+        if (m_isGenericClip && m_model != null) {
             string copyPath = SkillEditorConfig.GetAnimatorControllerCopyPath(m_model.name);
-            if (File.Exists(copyPath))
-            {
+            if (File.Exists(copyPath)) {
                 string sourcePath = SkillEditorConfig.GetAnimatorControllerPath(m_model.name);
                 sourcePath = SkillEditorTool.ProjectPathToFullPath(sourcePath);
                 File.Copy(copyPath, sourcePath, true);
@@ -148,15 +131,13 @@ public static class SkillEditorController
                 AssetDatabase.SaveAssets();
             }
         }
-        if (m_model)
-        {
+        if (m_model) {
             Object.DestroyImmediate(m_model);
             m_model = null;
         }
     }
 
-    public static void Exit()
-    {
+    public static void Exit() {
         Reset();
         EditorApplication.update = null;
         SkillEditorWindow.CloseWindow();
