340ba4a7023482b8597d07ffa8285529fd66c80f
NobuGaga <biweixiong@yinhandeiMac.local>
2019-11-11 22:25:32 +0800

    2019-11-11_Restructure Lua Logic Node

--- Assets/Editor/Script/CSharp/Lua/AnimClipData/FrameData.cs
+++ Assets/Editor/Script/CSharp/Lua/AnimClipData/FrameData.cs
@@ -23,9 +23,41 @@ namespace Lua.AnimClipData {
         public override string ToString() {
             return string.Empty;
         }
+
+        public void SetTableKeyValue(string key, object value) {
+            FrameType frameType = FrameType.None;
+            Enum.TryParse(key, false, out frameType);
+            switch (key) {
+                case Key_Name:
+                    SetFrameType(value as string);
+                    return;
+                case Key_Time:
+                    time = (float)value;
+                    return;
+                case Key_Priority:
+                    priority = (short)(int)value;
+                    return;
+                case Key_Data:
+                    dataList = value as CustomData[];
+                    return;
+            }
+        }
+
+        private static FieldKeyTable[] m_arraykeyValue;
+        public FieldKeyTable[] GetTableKeyValueList() {
+            if (m_arraykeyValue != null)
+                return m_arraykeyValue;
+            m_arraykeyValue = new FieldKeyTable[4];
+            m_arraykeyValue[0] = new FieldKeyTable(Key_Name, LuaFormat.ValueType.String);
+            m_arraykeyValue[1] = new FieldKeyTable(Key_Time, LuaFormat.ValueType.Number);
+            m_arraykeyValue[2] = new FieldKeyTable(Key_Priority, LuaFormat.ValueType.Int);
+            m_arraykeyValue[3] = new FieldKeyTable(Key_Data, LuaFormat.ValueType.Table);
+            return m_arraykeyValue;
+        }
     }
 
     internal enum FrameType {
+        None,
         Hit,
         PlayEffect,
         CacheBegin,
@@ -38,14 +70,6 @@ namespace Lua.AnimClipData {
         public float time;
         public short priority;
         public CustomData[] dataList;
-
-        public KeyFrameData(int index, FrameType frameType, float time, short priority, CustomData[] dataList) {
-            this.index = index;
-            this.frameType = frameType;
-            this.time = time;
-            this.priority = priority;
-            this.dataList = dataList;
-        }
 
         private void SetFrameType(string frameTypeString) {
             frameType = (FrameType)Enum.Parse(typeof(FrameType), frameTypeString);
@@ -76,44 +100,6 @@ namespace Lua.AnimClipData {
                                             priority,
                                             dataListString);
             return Tool.GetCacheString(toString);
-        }
-
-        public bool IsNullTable() {
-            return index == 0 || frameType == FrameType.None || priority == 0;
-        }
-
-        public void SetTableKeyValue(string key, object value) {
-            switch (key) {
-                case Key_Name:
-                    SetFrameType(value as string);
-                    return;
-                case Key_Time:
-                    time = (float)value;
-                    return;
-                case Key_Priority:
-                    priority = (short)(int)value;
-                    return;
-                case Key_Data:
-                    dataList = value as CustomData[];
-                    return;
-            }
-        }
-
-        private const string Key_Name = "name";
-        private const string Key_Time = "time";
-        private const string Key_Priority = "priority";
-        private const string Key_Data = "data";
-
-        private static FieldKeyTable[] m_arraykeyValue;
-        public FieldKeyTable[] GetTableKeyValueList() {
-            if (m_arraykeyValue != null)
-                return m_arraykeyValue;
-            m_arraykeyValue = new FieldKeyTable[4];
-            m_arraykeyValue[0] = new FieldKeyTable(Key_Name, LuaFormat.ValueType.String);
-            m_arraykeyValue[1] = new FieldKeyTable(Key_Time, LuaFormat.ValueType.Number);
-            m_arraykeyValue[2] = new FieldKeyTable(Key_Priority, LuaFormat.ValueType.Int);
-            m_arraykeyValue[3] = new FieldKeyTable(Key_Data, LuaFormat.ValueType.Table);
-            return m_arraykeyValue;
         }
     }
 }
