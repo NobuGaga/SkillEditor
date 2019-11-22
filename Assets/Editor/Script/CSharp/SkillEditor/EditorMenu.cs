38f6fea0fb5afd7cd6ffd8219ffcdd1b67e22053
NobuGaga <biweixiong@yinhandeiMac.local>
2019-11-22 09:45:49 +0800

    2019-11-22_Update Editor Menu Code

--- Assets/Editor/Script/CSharp/SkillEditor/EditorMenu.cs
+++ Assets/Editor/Script/CSharp/SkillEditor/EditorMenu.cs
@@ -5,28 +5,18 @@ namespace SkillEditor {
     internal static class EditorMenu {
 
         [MenuItem("技能编辑器/选择模型预设")]
-        private static void SelectPrefab() {
-            Manager.SelectPrefab();
-	    }
+        private static void SelectPrefab() => Manager.SelectPrefab();
 
         [MenuItem("技能编辑器/打开技能编辑窗口")]
-        private static void OpenWindow() {
-            EditorWindow.Open();
-	    }
+        private static void OpenWindow() => EditorWindow.Open();
 
         [MenuItem("技能编辑器/保存编辑到 Lua 文件 #S")]
-        private static void WriteAnimClipData() {
-            Manager.WriteAnimClipData();
-        }
+        private static void WriteAnimClipData() => Manager.WriteAnimClipData();
 
         [MenuItem("技能编辑器/复制本地布局文件到项目")]
-        private static void CopyLocalLayoutFileToProject() {
-            Manager.CopyLocalLayoutFileToProject();
-        }
+        private static void CopyLocalLayoutFileToProject() => Manager.CopyLocalLayoutFileToProject();
 
         [MenuItem("技能编辑器/退出编辑器模式")]
-        private static void Exit() {
-            Manager.Exit();
-        }
+        private static void Exit() => Manager.Exit();
     }
 }
