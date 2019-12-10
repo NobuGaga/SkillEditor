bf31b68718940a1a16c5c69f9708a6451a58a105
NobuGaga <biweixiong@yinhandeiMac.local>
2019-12-10 12:03:23 +0800

    2019-12-10_Add Batch Set Start Delay Time Component

---
+++ Assets/Editor/Script/CSharp/Inspector/ParticleDelay.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+using UnityEngine;
+using System;
+
+namespace SkillEditor {
+
+    [CustomEditor(typeof(ParticleDelay))]
+    public class ParticleDelayInspector : Editor {
+        
+        private const string BatchAddDelayTime = "批量增加 StartDelay";
+        private const string BatchSubDelayTime = "批量减少 StartDelay";
+
+        public override void OnInspectorGUI() {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button(BatchAddDelayTime))
+                SetParticlesDelay(AddDelayTime);
+            if (GUILayout.Button(BatchSubDelayTime))
+                SetParticlesDelay(SubDelayTime);
+            EditorGUILayout.EndHorizontal();          
+        }
+
+        private void SetParticlesDelay(Action<ParticleSystem, float> action) {
+            ParticleDelay delayComponent = target as ParticleDelay;
+            if (delayComponent == null)
+                return;
+            ParticleSystem[] particles = delayComponent.GetComponentsInChildren<ParticleSystem>();
+            if (particles == null || particles.Length == 0)
+                return;
+            float delayOffset = delayComponent.DelayTimeOffset;
+            for (ushort index = 0; index < particles.Length; index++)
+                action(particles[index], delayOffset);
+            EditorUtility.SetDirty(delayComponent.gameObject);
+            AssetDatabase.SaveAssets();
+        }
+
+        private void AddDelayTime(ParticleSystem particle, float delayTime) =>
+            particle.startDelay += delayTime;
+
+        private void SubDelayTime(ParticleSystem particle, float delayTime) =>
+            particle.startDelay -= delayTime;
+    }
+}
