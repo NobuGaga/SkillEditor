5778ac97d76e6be015baea107f297b24b83201a1
NobuGaga <biweixiong@yinhandeiMac.local>
2019-12-10 14:15:34 +0800

    2019-12-10_Add Particle Start Delay Component

--- Assets/Script/ParticleDelay.cs
+++ Assets/Script/ParticleDelay.cs
@@ -4,10 +4,9 @@ namespace SkillEditor {
 
     public class ParticleDelay : MonoBehaviour {
 
-        private const string InspectorName = "粒子特效偏移值";
+        private const string TipString = "粒子特效偏移值";
 
-        [SerializeField]
-        [InspectorName(InspectorName)]
+        [Tooltip(TipString)][Header(TipString)][SerializeField]
         private float m_delayTimeOffset = 0;
         public float DelayTimeOffset => m_delayTimeOffset;
     }
