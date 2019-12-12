0db125901ff7adbc01144d93338a00155e3d1e94
NobuGaga <biweixiong@yinhandeiMac.local>
2019-12-12 15:31:42 +0800

    2019-12-12_Update Runtime Cube Delay

--- Assets/Editor/Script/CSharp/SkillEditor/Model/Config.cs
+++ Assets/Editor/Script/CSharp/SkillEditor/Model/Config.cs
@@ -92,7 +92,7 @@ namespace SkillEditor {
 
         // Runtime
         public const float RuntimeEffectDelay = FramesPerSecond;
-        public const float RuntimeCubeDelay = FramesPerSecond;
+        public const float RuntimeCubeDelay = - FramesPerSecond;
         public const float DrawCubeLastTime = FramesPerSecond;
     }
 }
